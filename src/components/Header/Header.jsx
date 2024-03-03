d1615a361c3be7f76026c9289209bd77a2b087f4
lenatardis <olenaskuzmina@gmail.com>
2024-03-03 23:18:10 +0200

    Changed template

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
 
     return (
         <div className={'header'}>
-           <span>username</span>
+           <span>Telegram username</span>
             <span className={'username'}>
                 {user?.username}
             </span>
