cdc0538653065b6bba52291998da3ca84d5b4d25
lenatardis <olenaskuzmina@gmail.com>
2024-03-06 22:38:46 +0200

    Added styles for main page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,13 +2,28 @@ import styles from "./Main.module.scss";
 import {useSelector} from "react-redux";
 import Header from "./Header";
 import {getUser} from "../../store/selectors";
+import IconTether from "../../assets/images/tether.svg";
+import IconBitcoin from "../../assets/images/bitcoin.svg";
+import IconEtherium from "../../assets/images/etherium.svg";
+import IconLitecoin from "../../assets/images/litecoin.svg";
+import IconSomecoin from "../../assets/images/somecoin.svg";
+import {Link} from "react-router-dom";
 
 const Main = () => {
     const {currency, total, sign} = useSelector(getUser)[0];
+
+    const cryptoCurrencies = [
+        {icon: IconTether, name: 'USDT', value: '2878778.85', value2: ''},
+        {icon: IconBitcoin, name: 'BTC', value: '39945.3', value2: '45,423.00$'},
+        {icon: IconEtherium, name: 'ETH', value: ' 2222.08', value2: '2,453.41$'},
+        {icon: IconLitecoin, name: 'LTC', value: '65.76', value2: '69.27$'},
+        {icon: IconSomecoin, name: 'BNB', value: '319.84', value2: '319.8920$'}
+    ]
+
     return (
-        <div>
+        <div className={styles.main}>
             <Header back text="Main" menu/>
-            <div className={`${styles.main} wrap`}>
+            <div className="wrap">
                 <div className={`${styles['total-block']}`}>
                     <p>Total amount in wallets</p>
                     <div className={`${styles['currency-block']}`}>
@@ -16,6 +31,28 @@ const Main = () => {
                         <span className={styles.currency}>{currency.toUpperCase()}</span>
                     </div>
                 </div>
+            </div>
+            <div className={`${styles['currency-list']} wrap`}>
+                {cryptoCurrencies && (
+                    cryptoCurrencies.map((item, index) =>
+                        <div key={index}
+                             className={`${styles['currency-list__item']}`}>
+                            <div className={`${styles['title-block']}`}>
+                                <img className={styles.icon} src={item.icon} alt="{item.name}"/>
+                                <span>{item.name}</span>
+                            </div>
+                            <div className={`${styles['value-block']}`}>
+                                <div>
+                                    <span>{item.value}</span>
+                                    <span>{item.value2}</span>
+                                </div>
+                                <div>
+
+                                </div>
+                            </div>
+                        </div>
+                    )
+                )}
             </div>
         </div>
     )
