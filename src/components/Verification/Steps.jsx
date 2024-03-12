690036d147c07a558ea528d815accd426a82274c
lenatardis <olenaskuzmina@gmail.com>
2024-03-12 12:42:38 +0200

    Added correct steps color fill logic

--- src/components/Verification/Steps.jsx
+++ src/components/Verification/Steps.jsx
@@ -3,27 +3,27 @@ import styles from "./Steps.module.scss";
 
 const Steps = ({step}) => {
 
-    const steps = 3;
+    const totalSteps = 3;
+
+    const isActive = (itemStep) => parseInt(step) >= itemStep;
 
     return (
-        <div className={`${styles['steps-block']}`}>
+        <div className={styles['steps-block']}>
             <h3>Download and install the Google app Authenticator</h3>
-            <div className={`${styles['steps-row']}`}>
-                <div
-                    className={`${styles['steps-row__item']}  ${parseInt(step) === 1 ? styles['steps-row__item__active'] : ""}`}>
-                    <span>1</span>
-                </div>
-                <span className={styles.line}></span>
-                <div
-                    className={`${styles['steps-row__item']} ${parseInt(step) === 2 ? styles['steps-row__item__active'] : ""}`}>
-                    <span>2</span></div>
-                <span className={styles.line}></span>
-                <div
-                    className={`${styles['steps-row__item']} ${parseInt(step) === 3 ? styles['steps-row__item__active'] : ""}`}>
-                    <span>3</span></div>
+            <div className={styles['steps-row']}>
+                {Array.from({length: totalSteps}, (_, i) => (
+                    <React.Fragment key={i}>
+                        <div
+                            className={`${styles['steps-row__item']} ${isActive(i + 1) ? styles['steps-row__item__active'] : ""}`}>
+                            <span>{i + 1}</span>
+                        </div>
+                        {i < totalSteps - 1 && <span className={styles.line}></span>}
+                    </React.Fragment>
+                ))}
             </div>
         </div>
-    )
+    );
+
 }
 
 export default Steps;
