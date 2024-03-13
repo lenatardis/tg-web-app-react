f0660a0c60cbcc890c9c61291b9c9e76ad18e251
lenatardis <olenaskuzmina@gmail.com>
2024-03-13 10:38:20 +0200

    Added Nav/NoNav layout adjustment

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
+import { useLocation } from 'react-router-dom';
 
 const Layout = () => {
+    const location = useLocation();
+    const mainRoute = location.pathname.split('/')[1];
+    const noNavBarMainRoutes = ['account', 'verification'];
+    const isHomeRoute = location.pathname === '/';
+    const showNav = isHomeRoute || !noNavBarMainRoutes.includes(mainRoute);
+
     return (
-        <div className="inner-wrap">
+        <div className={`inner-wrap ${showNav ? '' : 'no-padding'}`}>
             <Outlet />
-            <Navigation />
+            {showNav && <Navigation />}
         </div>
     );
 };
