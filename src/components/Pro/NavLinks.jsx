34a51330efce95ca0edf8b0d8f3f1866125ca2e7
lenatardis <olenaskuzmina@gmail.com>
2024-03-25 12:30:23 +0200

    Added some exchange styles

--- src/components/Pro/NavLinks.jsx
+++ src/components/Pro/NavLinks.jsx
@@ -6,11 +6,13 @@ const NavLinks = ({subroute}) => {
     let navigate = useNavigate();
 
     return (
-        <div className={styles.row}>
-            <Link className={`${styles['nav-link']} ${subroute === 'exchange' ? styles['nav-link__active'] : ""}`} to='/pro/exchange'>
+        <div className={styles['navlink-row']}>
+            <Link className={`${styles['nav-link']} ${subroute === 'exchange' ? styles['nav-link__active'] : ""}`}
+                  to='/pro/exchange'>
                 <span>Exchange</span>
             </Link>
-            <Link className={`${styles['nav-link']} ${subroute === 'spot' ? styles['nav-link__active'] : ""}`} to='/pro/spot'>
+            <Link className={`${styles['nav-link']} ${subroute === 'spot' ? styles['nav-link__active'] : ""}`}
+                  to='/pro/spot'>
                 <span>Spot</span>
             </Link>
         </div>
--- src/components/Pro/Spot.jsx
+++ src/components/Pro/Spot.jsx
@@ -3,6 +3,8 @@ import Header from "../Common/Header/Header";
 import {Link, useLocation, useNavigate, useParams} from "react-router-dom";
 import useSubRoute from "../../hooks/useSubRoute";
 import NavLinks from "./NavLinks";
+import IconTether from "../../assets/images/tether_min.svg";
+import IconSelect from "../../assets/images/select.svg";
 import React from "react";
 
 const Spot = () => {
@@ -12,6 +14,16 @@ const Spot = () => {
             <Header back text="Exchange" menu/>
             <div className="wrap">
                 <NavLinks subroute={subroute}/>
+                <div className={styles.row}>
+                    <span>
+                        <img src={IconTether} alt=""/>
+                        <span>USDT/EUR</span>
+                        <span>344256.35</span>
+                        <img src={IconSelect} alt=""/>
+                    </span>
+                    <span>Buy</span>
+                    <span>Sell</span>
+                </div>
                 <p>Spot</p>
             </div>
         </div>
