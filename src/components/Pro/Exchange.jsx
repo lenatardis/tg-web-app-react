b61833c5386d5fca1c7c7700b5a61f5c38c57f09
lenatardis <olenaskuzmina@gmail.com>
2024-03-28 13:47:15 +0200

    Added exchange block on Exchange page

--- src/components/Pro/Exchange.jsx
+++ src/components/Pro/Exchange.jsx
@@ -1,9 +1,12 @@
-import styles from "./Pro.module.scss";
+import styles from "./Exchange.module.scss";
 import Header from "../Common/Header/Header";
-import {Link, useLocation, useNavigate, useParams} from "react-router-dom";
 import useSubRoute from "../../hooks/useSubRoute";
 import NavLinks from "./NavLinks";
 import React from "react";
+import IconEtherium from "../../assets/images/etherium.svg";
+import IconBitcoin from "../../assets/images/bitcoin.svg";
+import IconUp from "../../assets/images/up.svg";
+import IconVerticalSwitch from "../../assets/images/switch-vertical.svg";
 
 const Exchange = () => {
     let subroute = useSubRoute();
@@ -12,7 +15,51 @@ const Exchange = () => {
             <Header back text="Exchange" menu/>
             <div className="wrap">
                 <NavLinks subroute={subroute}/>
-                <p>Exchange</p>
+                <div className={`${styles['exchange-wrap']}`}>
+                    <div className={`${styles['exchange-block']}`}>
+                        <div className={`${styles['exchange-block__item']}`}>
+                            <div className={`${styles['title-row']}`}>
+                                <h2>Give</h2>
+                                <h2>Spot balanass ETH</h2>
+                            </div>
+                            <div>
+                                <span className={`${styles['number']}`}>0.00</span>
+                                <div>
+                                    <img src={IconEtherium} alt="" className={`${styles['currency-img']}`}/>
+                                    <span>Etherium</span>
+                                    <button>
+                                        <img src={IconUp} alt=""/>
+                                    </button>
+                                </div>
+                            </div>
+                            <div className={`${styles['availability-row']}`}>
+                                <p>2,655.08984 ETH available</p>
+                            </div>
+                        </div>
+                        <div className={`${styles['exchange-block__item']}`}>
+                            <div className={`${styles['title-row']}`}>
+                                <h2>Get</h2>
+                                <h2>Spot balanass BTC</h2>
+                            </div>
+                            <div>
+                                <span className={`${styles['number']}`}>0.00</span>
+                                <div>
+                                    <img src={IconBitcoin} alt="" className={`${styles['currency-img']}`}/>
+                                    <span>Bitcoin</span>
+                                    <button>
+                                        <img src={IconUp} alt=""/>
+                                    </button>
+                                </div>
+                            </div>
+                            <div className={`${styles['availability-row']}`}>
+                                <p>2,655.04 BTC available</p>
+                            </div>
+                        </div>
+                        <button className={`${styles['switch-button']}`}>
+                            <img src={IconVerticalSwitch} alt=""/>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
