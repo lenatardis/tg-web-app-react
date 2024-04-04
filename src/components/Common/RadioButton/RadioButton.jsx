3dc197136d2ac284126e7b346b8f6fe8bb672fd3
lenatardis <olenaskuzmina@gmail.com>
2024-04-04 18:06:51 +0300

    Modified Radio Button component

--- src/components/Common/RadioButton/RadioButton.jsx
+++ src/components/Common/RadioButton/RadioButton.jsx
@@ -1,16 +1,23 @@
 import styles from "./RadioButton.module.scss";
 
-const RadioButton = ({name, value, onSelect, selected}) => {
+const RadioButton = ({name, value, onSelect, selected, inner}) => {
 
     const handleChange = () => {
         onSelect(value);
     }
 
     return (
-       <div className={styles.wrap}>
-           <input type="radio" className={styles.radio} name={name} value={value} checked={selected === value} onChange={handleChange}/>
-           <p>{value}</p>
-       </div>
+        <div className={`${styles.wrap} ${inner ? styles.innerRadio : ''}`}>
+            <input type="radio" className={styles.radio} name={name} value={value} checked={selected === value}
+                   onChange={handleChange}/>
+            <p>
+                {inner && <span className={styles.valueBlock}>
+                   <span className={styles.circle}></span>
+                   <span>{value}</span>
+               </span>}
+                {!inner && value}
+            </p>
+        </div>
     );
 };
 
--- src/components/Wallet/Wallet.jsx
+++ src/components/Wallet/Wallet.jsx
@@ -1,12 +1,32 @@
 import styles from "./Wallet.module.scss";
 import Header from "../Common/Header/Header";
+import RadioButton from "../Common/RadioButton/RadioButton";
+import React, {useState} from "react";
 
 const Wallet = () => {
+    const [selectedOption1, setSelectedOption1] = useState('All');
+
+    const radioButtonInfo = {
+        option_1: [
+            {name: "option_1", value: "All"},
+            {name: "option_1", value: "Crypto"},
+            {name: "option_1", value: "Fiat"},
+        ]
+    }
+
     return (
         <div>
             <Header back text="Wallet" menu />
             <div className={`${styles.main} wrap`}>
                 <h2 className='title'>Wallet</h2>
+                <div className={styles.row}>
+                    {
+                        radioButtonInfo.option_1.map(({name, value}) => (
+                            <RadioButton key={value} name={name} value={value} selected={selectedOption1}
+                                         onSelect={setSelectedOption1} inner/>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
