374e093f95662cee86ca0df272f8a467195105d6
lenatardis <olenaskuzmina@gmail.com>
2024-04-11 17:08:13 +0300

    Added Settings popup styles and fixed some logic

--- src/components/Wallet/ManagerDepositAddresses/SettingsPopup/Popup.jsx
+++ src/components/Wallet/ManagerDepositAddresses/SettingsPopup/Popup.jsx
@@ -1,18 +1,40 @@
 import styles from "./Popup.module.scss";
 import ClosePopUp from "../../../Common/ClosePopUp/ClosePopUp";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Button from "../../../Common/Button";
 
 const SettingsPopUp = ({isVisible, closePopUp, name, address, index}) => {
+    const [addressName, setAddressName] = useState('');
+
+    useEffect(() => {
+        setAddressName(name);
+    }, [name]);
+
+    const handleNameChange = (e) => {
+        setAddressName(e.target.value);
+    }
+
     return (
-        <div className={`${styles['settings-popup']} ${styles['bottom-popup']} ${isVisible ? styles['popup-show'] : ''}`}>
+        <div
+            className={`${styles['settings-popup']} ${styles['bottom-popup']} ${isVisible ? styles['popup-show'] : ''}`}>
             <div className="resize">
                 <div className="wrap">
                     <ClosePopUp close={closePopUp}/>
                     <h2>Settings address</h2>
                     <div className={`${styles['settings-popup__inner-wrap']}`}>
-                        <h1>{name}</h1>
-                        <h2>{address}</h2>
-                        <h3>{index + 1}</h3>
+                        <div className={styles.block}>
+                            <h3>Index</h3>
+                            <p>{index + 1}</p>
+                        </div>
+                        <div className={styles.block}>
+                            <label htmlFor="wallet_address_name">Name the address</label>
+                            <input id="wallet_address_name" type="text" name="wallet_address_name" value={addressName} onChange={handleNameChange}/>
+                        </div>
+                        <div className={`${styles.block} ${styles.addressBlock}`}>
+                           <h3>Wallet address</h3>
+                            <p>{address}</p>
+                        </div>
+                        <Button text="Save" className={styles.saveBtn} handleClick={null}/>
                     </div>
                 </div>
             </div>
