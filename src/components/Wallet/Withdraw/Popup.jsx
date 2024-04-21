91a6ed8544fdb270aafd59464484dedcc0461e44
lenatardis <olenaskuzmina@gmail.com>
2024-04-21 23:24:56 +0300

    Added styles to Withdraw/Network pop-up

--- src/components/Wallet/Withdraw/Popup.jsx
+++ src/components/Wallet/Withdraw/Popup.jsx
@@ -13,13 +13,22 @@ const WithdrawNetwork = ({closePopUp, isVisible, selected, onSelect}) => {
             <div className="resize">
                 <div className="wrap">
                     <ClosePopUp close={closePopUp}/>
-                    <h2>Select network</h2>
-                    <div className={`${styles['popup__inner-wrap']}`}>
-                        {
-                            networks.map((el, index) => (
-                                <RadioButtonRow name="selected_network" value={el} key={index} selected={selected} onSelect={onSelect} networkrow/>
-                            ))
-                        }
+                    <div className={`${styles.currencyWrap} ${styles.block}`}>
+                        <div>
+                            <img src={src} alt=""/>
+                            <p>{name !== 'USDT' ? name : 'Tether USD'}</p>
+                        </div>
+                    </div>
+                    <div className={styles.block}>
+                        <h2>Select network</h2>
+                        <div className={`${styles['popup__inner-wrap']}`}>
+                            {
+                                networks.map((el, index) => (
+                                    <RadioButtonRow name="selected_network" value={el} key={index} selected={selected}
+                                                    onSelect={onSelect} networkrow/>
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
