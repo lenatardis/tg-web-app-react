f10fe1065eec35fd034c7014afefffe0d2a58726
lenatardis <olenaskuzmina@gmail.com>
2024-04-22 11:45:28 +0300

    Added some blocks to Withdraw/Currency page

--- src/components/Wallet/Withdraw/Currency.jsx
+++ src/components/Wallet/Withdraw/Currency.jsx
@@ -1,14 +1,29 @@
+import styles from "./Currency.module.scss";
 import Header from "../../Common/Header/Header";
-import {getCurrencyToWithdrawNetwork} from "../../../store/selectors";
+import {getCurrencyToWithdrawInfo, getCurrencyToWithdrawNetwork} from "../../../store/selectors";
 import {useSelector} from "react-redux";
 
 const CurrencyToWithdraw = () => {
+    let {name, balance, src} = useSelector(getCurrencyToWithdrawInfo)  ?? {};
+    let selectedNetwork = useSelector(getCurrencyToWithdrawNetwork);
+
     return (
         <div>
             <Header back text="Withdraw" menu/>
             <div className="wrap">
-                <p>Currency to withdraw</p>
-                <p>Network {useSelector(getCurrencyToWithdrawNetwork)}</p>
+                <div className={styles.currencyWrap}>
+                    <img src={src} alt=""/>
+                    <div>
+                        <div className={styles.currencyLine}>
+                            <p className={styles.name}>{name}</p>
+                            <span className={styles.networkWrap}><span>{selectedNetwork}</span></span>
+                        </div>
+                        <p>{balance} {name} available</p>
+                    </div>
+                </div>
+                <div className={styles.networkBlock}>
+                    <span>{selectedNetwork}</span>
+                </div>
             </div>
         </div>
     )
